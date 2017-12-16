34cc9b4e341f05d8c8c0243388dd8dd667caeca8
Mihran Mashhud <mihranmashhud@gmail.com>
2017-12-16 15:20:03 -0500

    Added modal

--- main.js
+++ main.js
@@ -1,54 +1,56 @@
 Vue.component("page", {
-  template: "#page"
+    template: "#page"
 });
 Vue.component("timeline", {
-  template: "#timeline"
+    template: "#timeline"
 });
 new Vue({
-  el: "#app",
-  data: () => ({
-    // fixed: false,
-    items: "",
-    title: "HighScheduler",
+    el: "#app",
+    data: () => ({
+        // fixed: false,
+        items: "",
+        title: "HighScheduler",
 
-    direction: "top",
-    fab: false,
-    fling: false,
-    hover: false,
-    tabs: null,
-    top: false,
-    right: true,
-    bottom: true,
-    left: false,
-    transition: "slide-y-reverse-transition",
-    fixed: true
-  }),
-  watch: {
-    top(val) {
-      this.bottom = !val;
+        dialog: false,
+
+        direction: "top",
+        fab: false,
+        fling: false,
+        hover: false,
+        tabs: null,
+        top: false,
+        right: true,
+        bottom: true,
+        left: false,
+        transition: "slide-y-reverse-transition",
+        fixed: true
+    }),
+    watch: {
+        top(val) {
+            this.bottom = !val;
+        },
+        right(val) {
+            this.left = !val;
+        },
+        bottom(val) {
+            this.top = !val;
+        },
+        left(val) {
+            this.right = !val;
+        }
     },
-    right(val) {
-      this.left = !val;
-    },
-    bottom(val) {
-      this.top = !val;
-    },
-    left(val) {
-      this.right = !val;
+    computed: {
+        activeFab() {
+            switch (this.tabs) {
+                case "one":
+                    return { class: "purple", icon: "account_circle" };
+                case "two":
+                    return { class: "red", icon: "edit" };
+                case "three":
+                    return { class: "green", icon: "keyboard_arrow_up" };
+                default:
+                    return {};
+            }
+        }
     }
-  },
-  computed: {
-    activeFab() {
-      switch (this.tabs) {
-        case "one":
-          return { class: "purple", icon: "account_circle" };
-        case "two":
-          return { class: "red", icon: "edit" };
-        case "three":
-          return { class: "green", icon: "keyboard_arrow_up" };
-        default:
-          return {};
-      }
-    }
-  }
 });
