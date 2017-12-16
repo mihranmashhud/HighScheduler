7bed57798f2da71f045e00a54240bec094b46724
Mihran Mashhud <mihranmashhud@gmail.com>
2017-12-16 11:31:58 -0500

    changed

---
+++ main.js
@@ -0,0 +1,16 @@
+Vue.component("page", {
+  template: "#page"
+});
+new Vue({
+  el: "#app",
+  data: {
+    clipped: false,
+    drawer: true,
+    fixed: false,
+    items: [{ icon: "bubble_chart", title: "Inspire" }],
+    miniVariant: false,
+    right: true,
+    rightDrawer: false,
+    title: "Vuetify.js"
+  }
+});
